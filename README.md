f738e2c9b3456aa77743bb485f9b65a150f21a8a
sumeet patil <sumeet.patil@sap.com>
2023-08-07 21:58:01 +0530

    Added template

--- README.md
+++ README.md
@@ -29,23 +29,28 @@ Fuzzing is a tool which injects random data into different protocol stacks. Fuzz
    ```
 3. Fuzzing help
    ```
-   ./mysecuritytool fuzzing --help 
-   Fuzzing url tool where the file line text will be appended at the end of url.
+   ./mysecuritytool fuzzing --help
+   Fuzzing tool where the file line text(generally passwords) will be appended/replaced. You can use {{.fuzz}} as a template where you can replace the passwords. You can use {{.fuzz}} in url, body, cookie or headers of the payload
 
    Usage:
-     mysecuritytool fuzzing [flags]
+   mysecuritytool fuzzing [flags]
 
    Flags:
-         --body string     Body for post
-         --cookie string   Cookie information
-         --file string     File name used for fuzzing
-     -h, --help            help for fuzzing
-         --regex string    Regex in fuzzing file line
-         --url string      Url to make call
+         --body string      Body for post
+         --cookie string    Cookie information
+         --file string      File name used for fuzzing
+         --headers string   Header information. You can pass like Content-Type=application/json;Accept:application/json
+   -h, --help             help for fuzzing
+         --regex string     Regex in fuzzing file line
+         --url string       Url to make call
    ```
-4. Example fuzzing command
+4. Examples of fuzzing command
    ```
    ./mysecuritytool fuzzing --file fuzz.txt --url https://URL --cookie "auth_token=test_token" --regex ".inc"
+
+   ./mysecuritytool fuzzing --file fuzz.txt --url https://your_url?name={{.fuzz}} --cookie "auth_token=test_token"
+
+   ./mysecuritytool fuzzing --file fuzz.txt --url https://your_url --body "{\"name\":\"{{.fuzz}}\"}" --cookie "auth_token=test_token"
    ```
 
 ### Fuzzing file references 
--- cmd/fuzzing.go
+++ cmd/fuzzing.go
@@ -12,10 +12,18 @@ import (
 	"github.com/sumeetpatil/mysecuritytool/internal/httpclient"
 )
 
+type Fuzzing struct {
+	Fuzz string
+}
+
 var fuzzingCmd = &cobra.Command{
 	Use:   "fuzzing",
 	Short: "Fuzzing",
-	Long:  `Fuzzing url tool where the file line text will be appended at the end of url.`,
+	Long: `Fuzzing tool where the file line text will be appended/replaced. 
+You can use {{.fuzz}} as a template where you can replace the passwords. You can use {{.fuzz}} in url, body, cookie or headers of the payload.
+
+Example:
+./mysecuritytool fuzzing --file fuzz.txt --url https://your_url?name={{.fuzz}} --cookie "auth_token=test_token"`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("fuzzing called")
 
@@ -29,27 +37,9 @@ var fuzzingCmd = &cobra.Command{
 			log.Fatalf("error - %s", err.Error())
 		}
 
-		headers, err := cmd.Flags().GetString("cookie")
+		headers, err := cmd.Flags().GetString("headers")
 		if err != nil {
 			log.Fatalf("error - %s", err.Error())
-		}
-
-		cookieMap := make(map[string]string)
-		if cookie != "" {
-			cookieData := strings.Split(cookie, ";")
-			for _, v := range cookieData {
-				cookieDataSplit := strings.Split(v, "=")
-				cookieMap[strings.TrimSpace(cookieDataSplit[0])] = strings.TrimSpace(cookieDataSplit[1])
-			}
-		}
-
-		headersMap := make(map[string]string)
-		if headers != "" {
-			headersData := strings.Split(cookie, ";")
-			for _, v := range headersData {
-				headersDataSplit := strings.Split(v, "=")
-				headersMap[strings.TrimSpace(headersDataSplit[0])] = strings.TrimSpace(headersDataSplit[1])
-			}
 		}
 
 		url, err := cmd.Flags().GetString("url")
@@ -81,6 +71,29 @@ var fuzzingCmd = &cobra.Command{
 		for scanner.Scan() {
 			line := scanner.Text()
 
+			cookieMap := make(map[string]string)
+			if cookie != "" {
+				cookie = replaceData(cookie, line)
+				cookieData := strings.Split(cookie, ";")
+				for _, v := range cookieData {
+					cookieDataSplit := strings.Split(v, "=")
+					cookieMap[strings.TrimSpace(cookieDataSplit[0])] = strings.TrimSpace(cookieDataSplit[1])
+				}
+			}
+
+			headersMap := make(map[string]string)
+			if headers != "" {
+				headers = replaceData(headers, line)
+				headersData := strings.Split(cookie, ";")
+				for _, v := range headersData {
+					headersDataSplit := strings.Split(v, "=")
+					headersMap[strings.TrimSpace(headersDataSplit[0])] = strings.TrimSpace(headersDataSplit[1])
+				}
+			}
+
+			url = replaceData(url, line)
+			body = replaceData(body, line)
+
 			if pattern != "" {
 				if regex.MatchString(line) {
 					call(url, line, cookieMap, headersMap, body)
@@ -96,6 +109,10 @@ var fuzzingCmd = &cobra.Command{
 
 		log.Fatalf("no success in fuzzing")
 	},
+}
+
+func replaceData(data string, fileLineText string) string {
+	return strings.ReplaceAll(data, "{{.fuzz}}", fileLineText)
 }
 
 func call(url string, line string, cookieMap map[string]string, headersMap map[string]string, body string) {
@@ -120,6 +137,7 @@ func init() {
 	fuzzingCmd.Flags().String("file", "", "File name used for fuzzing")
 	fuzzingCmd.Flags().String("url", "", "Url to make call")
 	fuzzingCmd.Flags().String("cookie", "", "Cookie information")
+	fuzzingCmd.Flags().String("headers", "", "Header information. You can pass like Content-Type=application/json;Accept:application/json")
 	fuzzingCmd.Flags().String("regex", "", "Regex in fuzzing file line")
 	fuzzingCmd.Flags().String("body", "", "Body for post")
 }
